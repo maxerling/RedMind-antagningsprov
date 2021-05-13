4b706d6048cdfa016e846617cd0ee436664a5268
maxerling <maximilianerling@gmail.com>
2021-05-13 08:54:58 +0200

    added more detail descp that works with JUnit 5

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,38 +17,48 @@ public class MainTest {
     @BeforeEach
     public void setUp() { atm = new ATM(4000,2,3,5);}
 
-    @Test
-    public void withdrawTest() {
-        String result = atm.withdraw(500);
-        assertEquals("Current amount: " + atm.getBalance() + "\n" +
-                "Thousand bills x 0" + "\n" +
-                "Five hundred bills x 1" + "\n" +
-                "Hundred bills x 0",result);
+    @Nested
+    @DisplayName("withdraw method")
+    class withdrawTest {
+        @Test
+        @DisplayName("Testing for viable withdraw")
+        public void withdrawTest() {
+            String result = atm.withdraw(500);
+            assertEquals("Current amount: " + atm.getBalance() + "\n" +
+                    "Thousand bills x 0" + "\n" +
+                    "Five hundred bills x 1" + "\n" +
+                    "Hundred bills x 0",result, "should return viable message");}
+
+        @Test
+        @DisplayName("Testing for greater than the balance amount")
+        public void withdrawGreaterThanBalanceTest() {
+            String result = atm.withdraw(5000);
+            assertEquals("Not enough balance, try again!",result, "should return nonviable message");
+        }
+
+        @Test
+        @DisplayName("Testing for less than the balance amount")
+        public void withdrawLessThanBalanceTest() {
+            String result = atm.withdraw(-500);
+            assertEquals("Not enough balance, try again!",result, "should return nonviable message");
+        }
+
+        @Test
+        @DisplayName("Testing for not available bill")
+        public void withdrawLessThanBillTest() {
+
+            String result = atm.withdraw(5);
+
+            assertEquals("Not enough balance, try again!",result, "should return nonviable message");
+        }
+
     }
 
-    @Test
-    public void withdrawGreaterThanBalanceTest() {
-        String result = atm.withdraw(5000);
-        assertEquals("Not enough balance, try again!",result);
-    }
-
-    @Test
-    public void withdrawLessThanBalanceTest() {
-        String result = atm.withdraw(-500);
-        assertEquals("Not enough balance, try again!",result);
-    }
-
-    @Test
-    public void withdrawLessThanBillTest() {
-
-        String result = atm.withdraw(5);
-
-        assertEquals("Not enough balance, try again!",result);
-    }
 
 
 
     @Test
+    @DisplayName("Testing the entire \"simulation\"")
     public void completeTest() {
         ATM atm2 = new ATM(4000,2,3,5);
 
@@ -67,7 +79,7 @@ public class MainTest {
 
 
         result = atm2.withdraw(400);
-        assertEquals("Not enough balance, try again!",result);
+        assertEquals("Not enough balance, try again!",result,"should return nonviable message");
 
 
         result = atm2.withdraw(1100);
@@ -80,7 +92,7 @@ public class MainTest {
 
         result = atm2.withdraw(1000);
 
-        assertEquals("Not enough balance, try again!",result);
+        assertEquals("Not enough balance, try again!",result,"should return nonviable message");
 
         result = atm2.withdraw(700);
 
@@ -91,15 +103,15 @@ public class MainTest {
 
         result = atm2.withdraw(300);
 
-        assertEquals("Not enough balance, try again!",result);
+        assertEquals("Not enough balance, try again!",result,"should return nonviable message");
 
-        assertEquals(0,atm2.getBalance());
-        assertNotEquals(100,atm2.getBalance());
+        assertEquals(0,atm2.getBalance(), "should return 0");
+        assertNotEquals(100,atm2.getBalance(), "should not return 100");
 
 
-        assertEquals(0,atm2.getThousandBills());
-        assertEquals(0,atm2.getFiveHundredBills());
-        assertEquals(0,atm2.getHundredBills());
+        assertEquals(0,atm2.getThousandBills(), "should return 0");
+        assertEquals(0,atm2.getFiveHundredBills(), "should return 0");
+        assertEquals(0,atm2.getHundredBills(),"should return 0");
     }
 
 
